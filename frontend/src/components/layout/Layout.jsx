146329f48a7e6d7cf1101e9d12fe2aab5f8004f3
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-13 13:46:27 -0500

    "replaced layout.jsx with tailwind version"

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import MainFeed from '../feed/MainFeed';
 import RightSidebar from './RightSidebar';
-import './Layout.css';
 
 function Layout() {
   return (
-    <div className="layout-container">
+    <div className="max-w-[1400px] mx-auto grid grid-cols-[280px_1fr_380px] gap-5 px-5 
+                    lg:grid-cols-[280px_1fr_380px] 
+                    md:grid-cols-[80px_1fr_300px] 
+                    sm:grid-cols-1">
       <Sidebar />
       <MainFeed />
       <RightSidebar />
